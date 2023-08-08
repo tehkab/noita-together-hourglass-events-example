a972784c40466d68304b45cb0c0c9c6bcd9647cf
Kabby <1925023+tehkab@users.noreply.github.com>
2023-08-08 19:46:18 -0400

    fix mod rename in files...

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --Append our translations to the game
 local TRANSLATIONS_FILE = "data/translations/common.csv"
-local translations = ModTextFileGetContent(TRANSLATIONS_FILE) .. ModTextFileGetContent("mods/test-nt-add-hourglass/files/translations/translations.csv")
+local translations = ModTextFileGetContent(TRANSLATIONS_FILE) .. ModTextFileGetContent("mods/noita-together-hourglass-events-example/files/translations/translations.csv")
 ModTextFileSetContent(TRANSLATIONS_FILE, translations)
 
 --Append hourglass events to noita together
-ModLuaFileAppend("mods/noita-together/files/scripts/hourglass_events.lua", "mods/test-nt-add-hourglass/files/append/hourglass_events.lua")
+ModLuaFileAppend("mods/noita-together/files/scripts/hourglass_events.lua", "mods/noita-together-hourglass-events-example/files/append/hourglass_events.lua")
